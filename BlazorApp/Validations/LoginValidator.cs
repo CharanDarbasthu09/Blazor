00596fad592f925dacbcaaf5d76865e05c6a4d72
Charan_Darbasthu <charan.darbasthu@agvance.net>
2024-01-23 19:58:34 +0530

    added fluent validation

---
+++ BlazorApp/Validations/LoginValidator.cs
@@ -0,0 +1,17 @@
+﻿using BlazorApp.Data;
+
+using FluentValidation;
+
+namespace BlazorApp.Validations
+{
+  public class LoginValidator : AbstractValidator<User>
+  {
+    public LoginValidator()
+    {
+      RuleFor(p => p.Email).NotEmpty()
+        .WithMessage("You must enter a valid email.")
+         .Matches(Constants.EmailRegex);
+      RuleFor(p => p.Password).NotEmpty().WithMessage("You must enter a valid password.");
+    }
+  }
+}
---
+++ BlazorApp/Validations/SignupValidator.cs
@@ -0,0 +1,18 @@
+﻿using BlazorApp.Data;
+
+using FluentValidation;
+
+namespace BlazorApp.Validations
+{
+  public class SignupValidator : AbstractValidator<User>
+  {
+    public SignupValidator()
+    {
+      RuleFor(p => p.Email).NotEmpty()
+      .WithMessage("You must enter a valid email.")
+      .Matches(Constants.EmailRegex);
+      RuleFor(p => p.UserName).NotEmpty().WithMessage("You must enter a valid user name");
+      RuleFor(p => p.Password).NotEmpty().WithMessage("You must enter a valid password.");
+    }
+  }
+}
